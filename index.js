61d8ac9a872ab273b3afc9b9e8ce39d4b2f5a39f
Christian Ureña <chrisureza@gmail.com>
2021-02-16 22:46:14 -0600

    searches model and show menu (WIP)

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
-const { inquirerMenu, pause } = require('./helpers/inquirer');
+const { inquirerMenu, pause, readInput } = require('./helpers/inquirer');
+const Searches = require('./models/searches');
 
 const main = async () => {
-    let opt;
+	let opt;
+	const searches = new Searches();
 
-    while (opt !== 0) {
-        opt = await inquirerMenu();
-        console.log({ opt });
-        opt !== 0 && await pause();
-    };
+	while (opt !== 0) {
+		opt = await inquirerMenu();
+
+		switch (opt) {
+			case 1:
+				const place = await readInput('City: ');
+				console.log(place);
+
+				console.log('\n-----Information-----');
+				console.log('City: ',);
+				console.log('Lat: ',);
+				console.log('Lng: ',);
+				console.log('Temp: ',);
+				console.log('Min: ',);
+				break;
+			case 2:
+
+				break;
+		}
+
+		opt !== 0 && await pause();
+	};
 };
 
 main();
---
+++ models/searches.js
@@ -0,0 +1,17 @@
+class Searches {
+    history = ['Tegucigalpa', 'Madrid', 'San Jose'];
+
+    constructor() {
+        // Read file if exists
+    }
+
+    async searchCity(city = '') {
+        // http request
+
+        console.log(city);
+
+        return [];
+    }
+}
+
+module.exports = Searches;
